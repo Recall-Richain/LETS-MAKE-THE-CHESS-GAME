1a75483f088e8af5d26fffd05ab90422ba9a3dfd
Me-meH <149717789+Recall-Richain@users.noreply.github.com>
2024-03-28 23:51:58 +0530

    Update chess.js
    the square is there now with color

--- chess.js
+++ chess.js
@@ -1,22 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const chessboard = document.getElementById("chessboard");
-    function createChessboard() {
-        let chessboardString = ''; 
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
-                
-                if (j % 2 === 0) {
-                    chessboardString += '#';
-                } else {
-                    chessboardString += '0';
-                }
-            }
-            chessboardString += '\n'; 
+  
+    for (let i= 0; i< 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        if ((i+j) % 2 === 0) {
+          square.classList.add("white");
+        } else {
+          square.classList.add("black");
         }
-        console.log(chessboardString); 
+        chessboard.appendChild(square);
+      }
     }
-
-    createChessboard();
-});
-
+  });
+  
 
